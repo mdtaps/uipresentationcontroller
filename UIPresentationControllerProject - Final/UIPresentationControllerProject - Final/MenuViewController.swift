4b3730e2597b32262d02f6ea86f849ee2cec5cf5
Mark Tapia <mdtaps@gmail.com>
2018-08-21 00:38:10 -0700

    feat: Duplicate and rename files to start completed project

---
+++ UIPresentationControllerProject - Final/UIPresentationControllerProject - Final/MenuViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MenuViewController.swift
+//  UIPresentationControllerProject - Final
+//
+//  Created by Mark Tapia on 8/20/18.
+//  Copyright © 2018 Mark Tapia. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    @IBAction func dismissTapped(_ sender: InspectableButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
